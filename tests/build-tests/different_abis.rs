86cc6015ad0d1b70ccac6f92f116182b618ec238
stashymane <me@stashy.dev>
2026-06-04 16:34:30 +0300

    split win32 tests into own module

--- tests/build-tests/different_abis.rs
+++ tests/build-tests/different_abis.rs
@@ -1,9 +1,7 @@
-#[cfg(target_os = "windows")]
 use retour_utils::hook_module;
 
-#[cfg(target_os = "windows")]
 #[hook_module("foo.bar")]
-mod hooks {
+mod shared {
     #[hook(NormalHook, symbol = "Foo")]
     fn normal_hook() -> i32 {
         NormalHook.call()
@@ -16,41 +14,41 @@ mod hooks {
         }
     }
 
-    #[cfg(all(target_family = "windows", target_arch = "x86"))]
-    #[hook(extern "cdecl" CDeclAbiHook, symbol = "Foo")]
-    fn cdecl_abi_hook() -> i32 {
-        CDeclAbiHook.call()
-    }
-
-    #[cfg(all(target_family = "windows", target_arch = "x86"))]
-    #[hook(extern "stdcall" StdCallAbiHook, symbol = "Foo")]
-    fn stdcall_abi_hook() -> i32 {
-        StdCallAbiHook.call()
-    }
-
-    #[cfg(all(target_family = "windows", target_arch = "x86"))]
-    #[hook(extern "fastcall" FastCallAbiHook, symbol = "Foo")]
-    fn fastcall_abi_hook() -> i32 {
-        FastCallAbiHook.call()
+    #[hook(extern "system" SystemAbiHook, symbol = "Foo")]
+    fn system_abi_hook() -> i32 {
+        SystemAbiHook.call()
     }
 
     #[hook(extern "C" CAbiHook, symbol = "Foo")]
     fn c_abi_hook() -> i32 {
         CAbiHook.call()
     }
+}
 
-    #[hook(extern "system" SystemAbiHook, symbol = "Foo")]
-    fn system_abi_hook() -> i32 {
-        SystemAbiHook.call()
+#[cfg(all(target_family = "windows", target_arch = "x86"))]
+#[hook_module("foo.bar")]
+mod win32 {
+    #[hook(extern "cdecl" CDeclAbiHook, symbol = "Foo")]
+    fn cdecl_abi_hook() -> i32 {
+        CDeclAbiHook.call()
     }
 
+    #[hook(extern "stdcall" StdCallAbiHook, symbol = "Foo")]
+    fn stdcall_abi_hook() -> i32 {
+        StdCallAbiHook.call()
+    }
+
+    #[hook(extern "fastcall" FastCallAbiHook, symbol = "Foo")]
+    fn fastcall_abi_hook() -> i32 {
+        FastCallAbiHook.call()
+    }
 
     // #[hook(extern "thiscall" ThisCallHook, symbol = "Foo")]
     // fn thiscall_abi_hook() -> i32 {
     //     ThisCallHook.call()
     // }
-
 }
+
 #[cfg(all(target_family = "windows", target_arch = "x86_64"))]
 #[hook_module("foo.bar")]
 mod win64 {
